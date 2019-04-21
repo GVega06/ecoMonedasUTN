1f536e9f213278d22b3343248c30d6465f5cb725
KEYLOR ALBERTO MARTINEZ RODRIGUEZ <komartinez@est.utn.ac.cr>
2019-04-21 16:14:10 -0600

    Billetera Virtual, Listado de Cupones e inicio de carrito

---
+++ EcomonedasUTN/Controllers/BilleteraVirtualController.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using EcomonedasUTN.Models;
+
+namespace EcomonedasUTN.Controllers
+{
+    public class BilleteraVirtualController : Controller
+    {
+        private ecoMonedaModel db = new ecoMonedaModel();
+
+        // GET: BilleteraVirtual
+        public ActionResult Index()
+        {
+            Usuario user = (Usuario)Session["session"];
+            if (user == null)
+            {
+                return RedirectToAction("InicioSesion","Usuario");
+            }
+            var lista = db.BilleteraVirtual.Where(x => x.idUsuario.Equals(user.email));
+            BilleteraVirtual v = null;
+
+            foreach(BilleteraVirtual item in lista)
+            {
+                if (item.idUsuario.Equals(user.email))
+                {
+                    v = item;
+                }
+            }
+            if(v == null)
+            {
+                v = new BilleteraVirtual();
+                v.idUsuario = user.email;
+                v.total = 0;
+                db.BilleteraVirtual.Add(v);
+                db.SaveChanges();
+            }
+            return View(v);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EcomonedasUTN/Models/Carrito.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EcomonedasUTN.Models
+{
+    public class Carrito
+    {
+        public List<CarritoCompras> Items { get; private set; }
+
+        //Implementación Singleton
+        //Las propiedades de solo lectura solo se pueden establecer en la inicialización o en un constructor
+        public static readonly Carrito Instancia;
+
+        //Se llama al constructor estatico tan pronto como la clase se carga en la memoria
+        static Carrito()
+        {
+            //Si el carrito no esta en la sesión, cree uno y guarde los items.
+            if (HttpContext.Current.Session["carrito"] == null)
+            {
+                Instancia = new Carrito();
+                Instancia.Items = new List<CarritoCompras>();
+                HttpContext.Current.Session["carrito"] = Instancia;
+            }
+            else
+            {
+                // De lo contrario, obtengalo de la sesión.
+                Instancia = (Carrito)HttpContext.Current.Session["carrito"];
+            }
+        }
+
+        //Un constructor protegido asegura que un objeto no se puede crear desde el exterior
+        protected Carrito() { }
+
+        
+        //AgregarItem (): agrega un material a la orden
+        public void AgregarItem(int elementoID)
+        {
+            // Crear un nuevo material para agregar al carrito
+            CarritoCompras nuevoItem = new CarritoCompras(elementoID);
+            // Si este material ya existe en lista de materiales, aumente la cantidad
+            // De lo contrario, agregue el nuevo elemento a la lista
+
+            if (Items.Exists(x => x.elementoID == elementoID))
+            {
+                CarritoCompras item = Items.Find(x => x.elementoID == elementoID);
+                item.Cantidad++;
+                return;
+            }
+            nuevoItem.Cantidad = 1;
+            Items.Add(nuevoItem);
+        }
+
+        //SetItemCantidad(): cambia la cantidad de un material en el carrito
+        public void SetItemCantidad(int elementoID, int cantidad)
+        {
+            // Si estamos configurando la cantidad a 0, elimine el material por completo
+            if (cantidad == 0)
+            {
+                EliminarItem(elementoID);
+                return;
+            }
+
+            // Encuentra el material y actualiza la cantidad
+            CarritoCompras actualizarItem = new CarritoCompras(elementoID);
+            if (Items.Exists(x => x.elementoID == elementoID))
+            {
+                CarritoCompras item = Items.Find(x => x.elementoID == elementoID);
+                item.Cantidad = cantidad;
+                return;
+            }
+        }
+
+        //EliminarItem (): elimina un Material del carrito de compras
+        public void EliminarItem(int elementoID)
+        {
+            if (Items.Exists(x => x.elementoID == elementoID))
+            {
+                var itemEliminar = Items.Single(x => x.elementoID == elementoID);
+                Items.Remove(itemEliminar);
+            }
+        }
+
+        public void limpiarCarrito()
+        {
+            Instancia.Items.Clear();
+        }
+
+        //GetSubTotal() - Devuelve el precio total de todos los materiales.
+        public decimal GetTotal()
+        {
+            decimal subTotal = 0;
+            foreach (CarritoCompras item in Items)
+                subTotal += item.subTotal();
+
+            return subTotal;
+        }
+
+        public int cantElementos()
+        {
+            int cant = 0;
+            foreach (CarritoCompras item in Items)
+            {
+                cant += item.Cantidad;
+            }
+            return cant;
+        }
+    }
+}
